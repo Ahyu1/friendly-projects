b5da0c521c9e36ab77efc9f9ebcfb9124460d37b
Ahyu1 <112178632+Ahyu1@users.noreply.github.com>
2023-10-19 19:05:28 +0700

    Create pass-generator.js

---
+++ password-genereator/pass-generator.js
@@ -0,0 +1,24 @@
+let button1 = document.getElementById("btn1")
+    let button2 = document.getElementById("btn2")
+
+    const characters = ["A","B","C","D","E","F","G","H","I","J","K",
+    "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+    "p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2",
+     "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$",
+     "%","^","&","*","(",")","_","-","+","=","{","[","}","]",","
+     ,"|",":",";","<",">",".","?","/"];
+
+    function generate() {
+        let length = 10
+        let randomPass1 = ""
+        let randomPass2 = ""
+        for (let i=0; i < length; i ++) {
+            randomPass1 += characters[Math.floor(Math.random() * characters.length)]
+            randomPass2 += characters[Math.floor(Math.random() * characters.length)]
+        }
+        button1.innerText = randomPass1
+        button2.innerText = randomPass2
+    }
+    
+    
